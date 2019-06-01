763d0ff840c6185642b1fb238415f0e8fd597de9
Erick Otsuka <erick.r.otsuka@gmail.com>
2019-06-01 20:30:56 -0300

    Add implementation of semaphore syscalls (not working)
     - There is a problem in the up syscall, possibly related to the
       queue.

--- sys_semaphore/sys_semaphore.c
+++ sys_semaphore/sys_semaphore.c
@@ -1,13 +1,39 @@
 #include <asm/barrier.h>
+#include <asm/current.h>
 #include <linux/linkage.h>
 #include <linux/list.h>
 #include <linux/module.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
 #include <linux/wait.h>
+#include <linux/interrupt.h>
 
 #define set_task_state(tsk, state_value)		\
 	do { (tsk)->state = (state_value); smp_mb(); } while (0)
+
+#define SEMAPHORE_MAX_INSTANCES (30)
+
+typedef struct __semaphore
+{
+	int id;
+	int counter;
+	wait_queue_head_t *queue;
+} semaphore_t;
+
+static int next_semaphore_id = 0;
+
+static semaphore_t semaphore_list[SEMAPHORE_MAX_INSTANCES];
+
+static int create_semaphore(int initial_counter)
+{
+	DECLARE_WAIT_QUEUE_HEAD(queue);
+	semaphore_t new_semaphore;
+	new_semaphore.id = next_semaphore_id;
+	new_semaphore.counter = initial_counter;
+	new_semaphore.queue = &queue;
+	semaphore_list[next_semaphore_id] = new_semaphore;
+	return next_semaphore_id++;
+}
 
 static int autoremove_autofree_wake_function(wait_queue_t *wait, unsigned mode, int sync, void *key)
 {
@@ -53,17 +79,31 @@ static long unblock(wait_queue_head_t *queue)
 
 asmlinkage long sys_init_semaphore(int initial_counter)
 {
-	return pr_info("syscall init semaphore called\n");
+	return create_semaphore(initial_counter);
 }
 
 asmlinkage long sys_up(int sem_id)
 {
-	return pr_info("syscall up called\n");
+	local_irq_disable();
+	semaphore_list[sem_id].counter++;
+	if (semaphore_list[sem_id].counter == 1)
+	{
+		unblock(semaphore_list[sem_id].queue);
+	}
+	local_irq_enable();
+	return 0;
 }
 
 asmlinkage long sys_down(int sem_id)
 {
-	return pr_info("syscall down called\n");
+	local_irq_disable();
+	if (semaphore_list[sem_id].counter == 0)
+	{
+		block(current->pid, semaphore_list[sem_id].queue);
+	}
+	semaphore_list[sem_id].counter--;
+	local_irq_enable();
+	return 0;
 }
 
 MODULE_LICENSE("GPL");
